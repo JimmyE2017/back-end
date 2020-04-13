fa2256ead5404a8b233f31392650a3a4a6678a79
pjouhaud <paul.jouhaud@withings.com>
2020-04-13 15:22:03 +0200

    Add workshop model

---
+++ app/models/workshop.py
@@ -0,0 +1,42 @@
+from __future__ import annotations
+import datetime
+from app.models.user_model import UserModel
+
+from app.models import db
+
+class Workshop(db.Document):
+    """
+    This is an abstract class.
+    Please inherit from it if you want to create a new type of workshop
+    """
+
+    meta = {"collection": "workshop", "allow_inheritance": True}
+
+    workshopId = db.StringField(primary_key=True)
+    title = db.StringField(required=True, max_length=128, min_length=1, default='Atelier CAPLC')
+    createdAt = db.DateTimeField(default=datetime.datetime.utcnow)
+    updatedAt = db.DateTimeField(default=datetime.datetime.utcnow)
+    startAt = db.DateTimeField(default=datetime.datetime.utcnow)
+    creatorId = db.StringField(db.ReferenceField(UserModel), required=True)
+    coachId = db.StringField(db.ReferenceField(UserModel), required=True)
+    eventUrl = db.StringField(default='caplc.com')
+    location = db.StringField(required=True, max_length=256, min_length=1, default='CAPLC')
+
+    def __repr__(self):
+        return f"<Workshop {self.workshopId} | {self.title} - animated by {self.coachId} at {self.location} on {self.startAt.strftime('%d/%m/%y')}>"
+
+    @classmethod
+    def find_by_id(cls, workshop_id: str) -> Workshop:
+        try:
+            workshop = cls.objects.get(workshopId=workshop_id)
+        except db.DoesNotExist:
+            workshop = None
+        return workshop
+
+    @classmethod
+    def find_by_coach_id(cls, coach_id: str) -> Workshop:
+        try:
+            workshop = cls.objects.get(coachId=coach_id)
+        except db.DoesNotExist:
+            workshop = None
+        return workshop
