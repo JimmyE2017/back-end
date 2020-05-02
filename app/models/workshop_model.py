6bd7d0303e18b18ed8ec9becebc333dc26faed2b
pjouhaud <paul.jouhaud@withings.com>
2020-05-02 17:25:43 +0200

    Update models

--- app/models/workshop_model.py
+++ app/models/workshop_model.py
@@ -1,10 +1,23 @@
 from __future__ import annotations
 
 import datetime
+from enum import Enum
 
 from app.common.uuid_generator import generate_id
 from app.models import db
 
+
+class ParticipantStatus(Enum):
+    CREATED = "created"
+    EXISTING = "existing"
+    FORMSENT = "formsent"
+    READY = "ready"
+
+
+class WorkshopParticipants(db.EmbeddedDocument):
+    from app.models.user_model import UserModel
+    userid = db.ReferenceField(UserModel)
+    status = db.StringField(max_length=32)
 
 class WorkshopModel(db.Document):
     """
@@ -12,7 +25,7 @@ class WorkshopModel(db.Document):
     Please inherit from it if you want to create a new type of workshop
     """
 
-    meta = {"collection": "workshops"}
+    meta = {"collection": "workshops", "allow_inheritance": True}
 
     workshopId = db.StringField(primary_key=True, default=generate_id)
     title = db.StringField(
@@ -26,11 +39,13 @@ class WorkshopModel(db.Document):
     eventUrl = db.StringField(default="caplc.com")
     city = db.StringField(max_length=128, min_length=1)
     address = db.StringField(max_length=512)
+    participants = db.ListField(db.EmbeddedDocumentField(WorkshopParticipants), default=[])
 
     def __repr__(self):
         return (
             f"<Workshop {self.workshopId} | {self.title} "
-            f"- animated by {self.coachId} at {self.city} on {self.startAt}>"
+            f"- animated by {self.coachId} at {self.city} on {self.startAt} "
+            f"- with {len(self.participants)} participants>"
         )
 
     @classmethod
@@ -48,3 +63,5 @@ class WorkshopModel(db.Document):
         except db.DoesNotExist:
             workshops = []
         return workshops
+
+
