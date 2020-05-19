09de0d3a444694d96ab96a6716e1e420a041e0e9
pjouhaud <paul.jouhaud@withings.com>
2020-05-19 23:35:45 +0200

    Add base model for persona

---
+++ app/models/persona.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+import datetime
+from flask import current_app
+from app.common.uuid_generator import generate_id
+from app.models import db
+
+
+class PersonaModel(db.Document):
+
+    meta = {"collection": "personas"}
+
+    id = db.StringField(primary_key=True, default=generate_id)
+    firstName = db.StringField(required=True, max_length=64, min_length=1)
+    lastName = db.StringField(required=True, max_length=64, min_length=1)
+    description = db.StringField()
+    createdAt = db.DateTimeField(default=datetime.datetime.utcnow)
+    updatedAt = db.DateTimeField(default=datetime.datetime.utcnow)
+    #compatibleModels = db.ListField(db.StringField(), default=[])
+    answers = db.DictField(required=True)
+
+
+    def __str__(self):
+        return f"Persona {self.id} - {self.firstName} {self.lastName}"
+
+    def __repr__(self):
+        return f"<Persona {self.id} - {self.firstName} {self.lastName}>"
+
+    @classmethod
+    def find_by_id(cls, id: str) -> PersonaModel:
+        try:
+            persona = cls.objects.get(id=id)
+        except db.DoesNotExist:
+            persona = None
+        return persona
